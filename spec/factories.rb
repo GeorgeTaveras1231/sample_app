99b2cd7a5d109d45efc1f972a01052fe139f1bfe
George Taveras <george.taveras1231@gmail.com>
2014-02-03 16:51:06 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
-  factory :user do
-    name                    "Stephanie Aviles"
-    email                   "steph.aviles@example.com"
-    password                "foobar"
-    password_confirmation   "foobar"
+  FactoryGirl.define do
+    factory :user do
+      sequence(:name)  { |n| "Person #{n}" }
+      sequence(:email) { |n| "person_#{n}@example.com"}
+      password "foobar"
+      password_confirmation "foobar"
+    
+      factory :admin do
+        admin true
+      end
+    end
   end
+  
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -4,3 +4,29 @@ Dir[Rails.root.join("/app/helpers/**_helper.rb")].each do |f|
 end
 =end
 include ApplicationHelper
+
+def valid_signin(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
+RSpec::Matchers.define :have_error_message do |message = ''|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message = ''|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
